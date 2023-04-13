02225332c62c65e2c8c1c783af65fedb3c83e4c8
snr02 <2020csm.r53@svce.edu.in>
2023-04-13 18:51:22 +0530

    demo commi

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,4 +13,4 @@ const store = configureStore({
 
 export default store;
 
-export const server = "https://edumindz-backend.vercel.app/api/v1";
+export const server = "https://edumindz-backend.vercel.app/";
